01fd20171224ea911f604390c55927db0d3910b7
john <johnfrichmagan530@gmail.com>
2025-05-08 13:42:25 +0800

    Updates

--- assets/ios.transition-legacy-6IzV2xO7.js
+++ assets/ios.transition-legacy-6IzV2xO7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bp8d2OA5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BIUoA3Dj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-DgE0aMnK.js
+++ assets/status-tap-legacy-DgE0aMnK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bp8d2OA5.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-BIUoA3Dj.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
